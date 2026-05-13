8cefdb8b0c6f88bfeaedb662127615b76acc061c
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-05-13 10:53:47 -0500

    localStorage added

--- script.js
+++ script.js
@@ -2,15 +2,40 @@ const formNote = document.querySelector("form");
 const inputNote = formNote.querySelector(".input");
 const notes = document.querySelector(".notes");
 
+document.addEventListener("DOMContentLoaded", () => {
+    const getData = localStorage.getItem("data");
+    if(getData && getData.trim() != "") {
+        notes.innerHTML = getData;
+    };
+});
+
 formNote.addEventListener("submit", (e) => {
     e.preventDefault();
     let data = inputNote.value.trim();
     inputNote.value = "";
-    const liNote = document.createElement("li");
-    liNote.innerHTML = `<p>${data}</p> <button class="deleteNote">Delete</button>`
-    const deleteNote = liNote.querySelector(".deleteNote");
-    deleteNote.addEventListener("click", () => {
-        notes.removeChild(liNote);
-    });
-    notes.appendChild(liNote);
+    if(data != "") {
+        createNote(notes, data);
+    } else { 
+        return;
+    }
 });
+
+function createNote(notes, data) {
+    notes.innerHTML += `
+    <li class="liNote">
+        <button class="deleteNote">x</button>
+        <p>${data}</p>
+    </li>
+    `
+    localStorage.setItem("data", notes.innerHTML);
+};
+
+notes.addEventListener("click", (e) => {
+    if (e.target.classList.contains("deleteNote")) {
+        const liNote = e.target.parentElement;
+        notes.removeChild(liNote);
+        localStorage.setItem("data", notes.innerHTML);
+    };
+});
+
+
